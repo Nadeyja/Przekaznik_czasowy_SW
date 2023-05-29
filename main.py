a5f5e4a5e84525d3c39653aec9412da78013fc61
Nadeyja <nadia.hamerlak@gmail.com>
2023-05-30 00:32:42 +0200

    Added standard menu

--- main.py
+++ main.py
@@ -16,5 +16,21 @@ def auto():
     pass
 
 
-print("Wcisnij przycisk")
-manual()
+while True:
+    #print("Przekaźnik czasowy PCR-515 Duo z opóźnionym załączeniem\n")
+    #print("Po podaniu napięcia zasilającego styk pozostaje w pozycji 11-10 i następuje odmierzanie nastawionego czasu pracy. ")
+    #print("Po odmierzeniu czasu następuje przełączenie styku w pozycję 11-12. ")
+    #print("Ponowna realizacja trybu pracy przekaźnika możliwa jest po odłączeniu napięcia zasilającego i ponownym jego załączeniu. \n")
+    while True:
+        print("1. MANUAL")
+        print("2. AUTO")
+        x = input("Wybierz opcję: ")
+        if x == "1":
+            manual()
+            pass
+        elif x == "2":
+            auto()
+            pass
+        else:
+            print("Nie ma takiej opcji, wpisz ponownie.\n")
+
